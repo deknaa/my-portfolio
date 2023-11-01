fbac6ef77d0cad42503acf1b05aef4259d194931
deknaa <kadekna20@gmail.com>
2023-11-01 21:17:39 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-blue-600 text-stone-300 rounded-lg py-2 px-5 m-5'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
