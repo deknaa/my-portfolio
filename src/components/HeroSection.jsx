55691894b107dc86d0630558847aa5c57d5a9111
deknaa <kadekna20@gmail.com>
2024-05-03 15:40:26 +0800

    style(hero): change layout

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,22 +3,12 @@ import deknaImage from '../assets/images/dekna.png'
 
 const HeroSection = () => {
   return (
-    <div className='bg-thirdColor'>
-
-    <div className='flex items-center flex-col pt-[20%] md:ms-[20%] md:flex-row md:pt-0'>
-
-      <div className='w-1/2'>
-        <h1 className='text-white text-6xl'>Hi I'm <br /><span className='font-bold text-secondaryColor'>Kadek Krisna</span></h1>
-        <p className='text-white mt-4'>I am a student who likes the world of IT, I really like learning new things <br />especially in my hard skills and soft skills, I have the desire to become a <br /> software engineer</p>
-        <a href="#" className='bg-secondaryColor text-primaryColor p-2 rounded-lg inline-block mt-3'>See My Work</a>
+    <div className='bg-thirdColor flex justify-center items-center h-[85vh]'>
+      <div className='flex flex-col ms-[10%] me-[10%]'>
+        <h1 className='text-white text-5xl md:text-6xl text-center'>Hi I'm <br /><span className='text-secondaryColor'>Kadek Krisna</span></h1>
+        <p className='text-white text-sm md:text-base mt-3 text-center'>I am a student who likes the world of IT, I really like learning new things <br />especially in my hard skills and soft skills, I have the desire to become a <br /> software engineer</p>
+        <a href="#" className='bg-secondaryColor text-primaryColor p-2 rounded-lg inline-block mt-7 text-center font-bold'>See My Work</a>
       </div>
-
-      <div className='w-1/2 text-white'>
-        <img src={deknaImage} alt="" />
-      </div>
-
-    </div>
-
     </div>
   )
 }
