d14304182e4b747e9a0709c2c70756b45b4f9405
deknaa <kadekna20@gmail.com>
2024-05-03 17:01:33 +0800

    style(hero): added gradient color on button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className='flex flex-col ms-[10%] me-[10%]'>
         <h1 className='text-white text-5xl md:text-6xl text-center'>Hi I'm <br /><span className='text-secondaryColor'>Kadek Krisna</span></h1>
         <p className='text-white text-sm md:text-base mt-3 text-center'>I am a student who likes the world of IT, I really like learning new things <br />especially in my hard skills and soft skills, I have the desire to become a <br /> software engineer</p>
-        <a href="#" className='bg-secondaryColor text-primaryColor p-2 rounded-lg inline-block mt-7 text-center font-bold'>See My Work</a>
+        <a href="#" className=' text-primaryColor p-2 rounded-lg inline-block mt-7 text-center font-bold bg-gradient-to-r from-secondaryColor to-white hover:text-secondaryColor hover:bg-gradient-to-r hover:from-primaryColor hover:to-white transition duration-500'>See My Work</a>
       </div>
     </div>
   )
