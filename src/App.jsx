dce0c33e25b508e27bc8fe906ea48c3c52929c56
deknaa <kadekna20@gmail.com>
2024-05-03 11:56:34 +0800

    project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
